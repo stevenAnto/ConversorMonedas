e1f96547bf7d7f02bf499218f1562fc46032e92d
stevenAnto <ecalcinap@unsa.edu.pe>
2024-05-25 18:32:22 -0500

    consumiendo api ok

---
+++ src/modelos/Cambiar.java
@@ -0,0 +1,31 @@
+package modelos;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Cambiar {
+    public Data cambio(String baseCurrency, String quoteCurrency, double amount) {
+        String url = "https://v6.exchangerate-api.com/v6/7e95e36516d214f6bff687ec/pair/"+
+                baseCurrency+"/"+quoteCurrency+"/"+amount;
+        System.out.println("url es "+ url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        HttpResponse<String> response;
+        try {
+            response = client.send(request,HttpResponse.BodyHandlers.ofString());
+            System.out.println("response "+ response.body());
+            return  new Gson().fromJson(response.body(), Data.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/modelos/Data.java
@@ -0,0 +1,4 @@
+package modelos;
+
+public record Data(String conversion_result) {
+}
